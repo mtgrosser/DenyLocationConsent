5f0a433f3de564b1d41306e474c5a734645cefb3
Matthias Grosser <mtgrosser@gmx.net>
2018-04-06 23:48:35 +0200

    Update for Play Services 12.5.21 #13

--- src/org/brainkiller/xposed/denylocationconsent/DenyLocationConsent.java
+++ src/org/brainkiller/xposed/denylocationconsent/DenyLocationConsent.java
@@ -3,6 +3,8 @@ package org.brainkiller.xposed.denylocationconsent;
 import android.content.Context;
 import android.location.LocationManager;
 import android.os.Build;
+import android.provider.Settings.Secure;
+import android.util.Log;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodReplacement;
@@ -14,15 +16,15 @@ public class DenyLocationConsent implements IXposedHookLoadPackage {
     public void handleLoadPackage(LoadPackageParam lpparam) throws Throwable {
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             if (lpparam.packageName.equals("com.google.android.gms")) {
-                final Class<?> NetworkLocationChimeraService = XposedHelpers.findClass("com.google.android.location.network.NetworkLocationChimeraService", lpparam.classLoader);
+                final Class<?> NetworkLocationProvider = XposedHelpers.findClass("com.google.android.location.network.NetworkLocationProvider", lpparam.classLoader);
 
-                XposedHelpers.findAndHookMethod("com.google.android.location.network.NetworkLocationChimeraService", lpparam.classLoader, "a", Context.class, new XC_MethodReplacement() {
+                XposedHelpers.findAndHookMethod("com.google.android.location.network.NetworkLocationProvider", lpparam.classLoader, "a", new XC_MethodReplacement() {
                     @Override
                     protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
-                        Context context = (Context) param.args[0];
-
-                        XposedHelpers.callStaticMethod(NetworkLocationChimeraService, "b", context, false);
-
+                        Context context = (Context) XposedHelpers.getObjectField(param.thisObject, "a");
+                        Secure.setLocationProviderEnabled(context.getContentResolver(), "network", false);
+                        Log.wtf("GmsNetworkLocationProvi", "Don't be evil");
+                        
                         return null;
                     }
                 });
